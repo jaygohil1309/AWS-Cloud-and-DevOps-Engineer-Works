b43dc31acc3f1636ec4f550c79c952c3d5ed00ee
jaygohil1309 <gohiljayb10@gmail.com>
2024-03-13 22:52:57 +0530

    add new file

--- Python-for-DevOps/Day-2/Test.py
+++ Python-for-DevOps/Day-2/Test.py
@@ -1,7 +1,9 @@
-arn = "arn:aws:iam::123456789012:user/johndoe"
+# arn = "arn:aws:iam::123456789012:user/johndoe"
  
 # Question :- Print the only User_name and only arn....
 
-print(arn.split("/")[1])
+# print(arn.split("/")[1])
 
+name = "jay gohil" # Convert into Upper_Case.....
 
+print(name.upper())
