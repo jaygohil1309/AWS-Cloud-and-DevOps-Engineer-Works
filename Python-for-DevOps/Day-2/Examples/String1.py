cc839eb6549dd07f4405f3f5cf60217bc9cbb404
jaygohil1309 <gohiljayb10@gmail.com>
2024-03-14 23:39:49 +0530

    add code

---
+++ Python-for-DevOps/Day-2/Examples/String1.py
@@ -0,0 +1,30 @@
+"""
+
+*** String_Concatenation_examples.....
+
+1) Using the + operator
+2) Using the join method
+3) Using f-strings method
+
+"""
+
+# Example :- 1 => Using the " + " Operator....
+
+# str1 = "Hello"
+# str2 = "World"
+
+# result = str1 + " " + str2
+# print(result)
+
+# Example :- 2 => Using the Join method....
+
+# str = "Hello", "World!!"
+# result1 = " ".join(str)
+# print(result1)
+
+# Example :- 3 => Using f-string method....
+
+str1 = "Hello"
+str2 = "World!!"
+result = f"{str1} {str2}"
+print(result)
