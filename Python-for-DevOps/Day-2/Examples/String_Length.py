c69cf62ac923678f64281d4f78ff712d8e2254bc
jaygohil1309 <gohiljayb10@gmail.com>
2024-03-14 23:50:07 +0530

    add code

---
+++ Python-for-DevOps/Day-2/Examples/String_Length.py
@@ -0,0 +1,14 @@
+"""
+
+*** Python_Length :- The " len() " function is used to determine the length of the string
+                     'my_string'. The length is then printed to the console.
+
+"""
+
+# Example :- 1
+
+my_string = "Python is awesome"
+
+length = len(my_string)
+
+print(length)
