4d45bd05cb071138ec138349135959e31289af0e
jaygohil1309 <gohiljayb10@gmail.com>
2024-03-14 22:12:44 +0530

    add code

---
+++ Python-for-DevOps/Day-2/DataTypes.py
@@ -0,0 +1,50 @@
+"""
+*** Data Types in Python :- 
+
+==>> In programming, a data type is a classification or categorization that specifies 
+     which type of a value a variable can hold. Data types are essantial because they
+     determine how data is stored in memory and what operations can be performed on that
+     data. Python, like many programming languages, supports several built-in data types.
+     Here are some of the common data types in python.....
+
+"""
+# 1) Numeric Data Types :- int, float, complex....
+
+x = 5 # int_type
+y = 3.14 # Float_type
+z = 2 + 3j # Complex_type
+
+# 2) Sequence Types :- str, list, tuple....
+
+text = "Hello, World" # Str_type
+my_list = [1,2,3] # List_type
+my_tuple = (1,2,3) # Tuple_type
+
+# 3) Mapping Type :- dict......
+
+my_dict = {1:"Jay", 2:"Vishvam", 3:"Kathan"} # Dict_type
+
+# 4) Set Types :- Set, Frozenset.....
+
+my_set = {1,2,3}
+my_frozenset = frozenset([1,2,3])
+
+# 5) Boolean Type :- bool [True or False].....
+
+is_valid = True
+
+# 6) Binary Types :- bytes, bytearray.....
+
+data = b'Hello' # bytes.....
+data = bytearray(b'Hello') # bytearray....
+
+# 7) None Type :- Represent the [None] object, which is used to indicate the absence
+#                 of a value or a null value. 
+
+# 8) Custom Data Types :- You can also define your custom data types using classes and objects.
+
+                  
+              
+
+
+
