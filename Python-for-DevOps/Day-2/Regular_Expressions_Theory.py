4731174dc55a83ef7cb1ceed384a5a8c51570398
jaygohil1309 <gohiljayb10@gmail.com>
2024-03-15 21:41:59 +0530

    add new file

---
+++ Python-for-DevOps/Day-2/Regular_Expressions_Theory.py
@@ -0,0 +1,16 @@
+"""
+
+*** Regex
+
+1) Regular expression for text Processing :-
+
+* Regular expressions (regex or regexp) are a powerful tool for pettern matching and text
+  processing.
+
+* The [re] module in python is used for working with regular expressions.
+
+* Common metacharacters :- [.]=> Any character, [*]=> One or more, [?]=> Zero or one,
+                           []=> Character class, [|]=> OR, [^]=> Start of a line,
+                           [$]=> end of a line etc..........
+
+"""
