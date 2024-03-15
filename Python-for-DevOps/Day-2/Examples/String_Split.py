6de5bd724d4728090cf42d4a54797001580afaea
jaygohil1309 <gohiljayb10@gmail.com>
2024-03-15 21:18:28 +0530

    add changes

--- Python-for-DevOps/Day-2/Examples/String_Split.py
+++ Python-for-DevOps/Day-2/Examples/String_Split.py
@@ -1,3 +1,12 @@
+
+"""
+
+*** String_Split() :- The split() method splits a string into a list of substrings based
+                        on a specified delimiter.
+
+"""
+
+
 text = "Python is awesome"
 
 words = text.split()
