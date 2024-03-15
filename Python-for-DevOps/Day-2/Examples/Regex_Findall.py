c04521cee3e9ee1ba8b15457d676ca684c8eda7a
jaygohil1309 <gohiljayb10@gmail.com>
2024-03-15 21:53:31 +0530

    add new file

--- Python-for-DevOps/Day-2/Examples/Regex_Findall.py
+++ Python-for-DevOps/Day-2/Examples/Regex_Findall.py
@@ -3,9 +3,16 @@ import re
 text = "The quick brown fox"
 pattern = r"brown"
 
-search = re.search(pattern, text)
+# search = re.search(pattern, text)
 
-if search:
-    print("Pattern found:", search.group())
+# if search:
+#     print("Pattern found:", search.group())
+# else:
+#     print("Pattern not found")
+
+matches = re.findall(pattern, text)
+
+if matches:
+    print("Pattern found:", matches)
 else:
     print("Pattern not found")
