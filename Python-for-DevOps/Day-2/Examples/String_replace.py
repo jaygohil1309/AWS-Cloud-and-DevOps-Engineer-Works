d342dbdcb18b8df016129a671cbe2d9421d94400
jaygohil1309 <gohiljayb10@gmail.com>
2024-03-16 23:49:37 +0530

    add changes

--- Python-for-DevOps/Day-2/Examples/String_replace.py
+++ Python-for-DevOps/Day-2/Examples/String_replace.py
@@ -3,3 +3,9 @@ text = "Python is awesome"
 new_text = text.replace("awesome", "Great")
 
 print("Modified text:-", new_text)
+
+
+# str = "Jay gohil"
+
+# replace_string = str.replace("gohil", "Gohil")
+# print(replace_string)
