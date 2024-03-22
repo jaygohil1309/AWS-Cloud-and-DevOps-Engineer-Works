6864f3b01b5a6576fe8565d509ce81a55a4ad736
jaygohil1309 <gohiljayb10@gmail.com>
2024-03-22 22:03:07 +0530

    add code file

--- Python-for-DevOps/Day-4/Examples_Functions/Calculator.py
+++ Python-for-DevOps/Day-4/Examples_Functions/Calculator.py
@@ -1,10 +1,10 @@
-num1 = 10
-num2 = 5
+# num1 = 10
+# num2 = 5
 
-addition = num1 + num2
-print("Value of addition:-" +  addition) # TypeError: can only concatenate str (not "int")
-                                      # to  str
+# addition = num1 + num2
+# print("Value of addition:-" +  addition) # TypeError: can only concatenate str (not "int")
+#                                       # to  str
 
-print("Value of addition:-" + str(addition)) # This is right method to concatenate(string).
+# print("Value of addition:-" + str(addition)) # This is right method to concatenate(string).
 
 
