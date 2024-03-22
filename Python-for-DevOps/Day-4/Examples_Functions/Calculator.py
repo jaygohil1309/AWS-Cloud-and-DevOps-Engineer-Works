f50907c55ff1058e6f36e08469812c901ec5eecd
jaygohil1309 <gohiljayb10@gmail.com>
2024-03-22 22:17:38 +0530

    add code file

--- Python-for-DevOps/Day-4/Examples_Functions/Calculator.py
+++ Python-for-DevOps/Day-4/Examples_Functions/Calculator.py
@@ -7,4 +7,16 @@
 
 # print("Value of addition:-" + str(addition)) # This is right method to concatenate(string).
 
+num1 = 10
+num2 = 5
 
+def addition():
+    result = num1 + num2
+    print(result)
+
+def multiplication():
+    result1 = num1 * num2
+    print(result1)
+
+addition() # Calling the function......
+multiplication()
