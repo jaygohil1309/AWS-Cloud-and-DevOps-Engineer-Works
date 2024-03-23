eb3dbf81567bd37c50b91585a6fbb2c63080c01d
jaygohil1309 <gohiljayb10@gmail.com>
2024-03-23 21:29:32 +0530

    add new code file

---
+++ Python-for-DevOps/Day-4/Examples/Module1.py
@@ -0,0 +1,9 @@
+
+# You can use this module in another script:-
+
+import Module_Example
+
+result = Module_Example.square(5)
+print(result)
+
+print(Module_Example.pi)
---
+++ Python-for-DevOps/Day-4/Examples/Module_Example.py
@@ -0,0 +1,8 @@
+
+# suppose you have a Python file named "Module_Example.py":-
+
+def square(x):
+    return x ** 2
+
+pi = 3.14159265
+
