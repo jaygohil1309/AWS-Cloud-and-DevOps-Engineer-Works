919ebfa4cddb43e9bac0225432335b5a5b1162cf
jaygohil1309 <gohiljayb10@gmail.com>
2024-03-23 21:22:16 +0530

    add code file

---
+++ Python-for-DevOps/Day-4/Examples/Function_Example.py
@@ -0,0 +1,17 @@
+
+# Function_Example :-
+
+def greet(name):
+    return f"Hello, {name}!"
+
+message = greet("Alice")
+print(message)
+
+"""
+
+In this example, "greet" is a function that takes a "name" argument and returns a 
+    greeting message.
+
+"""
+
+
