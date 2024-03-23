b5f4015b3882b5fe683d00c5e51c8f10f80786db
jaygohil1309 <gohiljayb10@gmail.com>
2024-03-23 22:23:33 +0530

    add code file

---
+++ Python-for-DevOps/Day-4/Examples/Packages.py
@@ -0,0 +1,22 @@
+"""
+
+my_package/
+
+    __init__.py
+    module1.py
+    module2.py
+
+"""
+# You can use modules from this package as follow :-
+
+from my_package import module1
+
+result = module1.function_from_module1()
+
+"""
+
+In this example, "my_package" is a python package containing modules "module1" and
+"module2".
+
+"""
+
