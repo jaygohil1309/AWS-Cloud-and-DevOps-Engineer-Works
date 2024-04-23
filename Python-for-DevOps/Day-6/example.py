2e8a3dae0dd36254721b4f461c91f5fdc09f6a77
jaygohil1309 <gohiljayb10@gmail.com>
2024-04-23 22:08:55 +0530

    add new changes

--- Python-for-DevOps/Day-6/example.py
+++ Python-for-DevOps/Day-6/example.py
@@ -1,3 +1,11 @@
-x = 10
-x += 5
-print(x)
+# x = 10 # Assignment Operations
+# x += 5
+# print(x)
+
+# Bitwise Operations
+
+x = 5 # Binary : 0101
+y = 3 # Binary : 0011
+
+result = x & y
+print(result) # Result: 0001 (Deciman : 1)
