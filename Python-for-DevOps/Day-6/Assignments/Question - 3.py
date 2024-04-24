33a60d2f4426a2e4d0917c8c748a5d8b851b8ed1
jaygohil1309 <gohiljayb10@gmail.com>
2024-04-24 23:03:07 +0530

    add new changes

--- Python-for-DevOps/Day-6/Assignments/Question - 3.py
+++ Python-for-DevOps/Day-6/Assignments/Question - 3.py
@@ -1,2 +1,19 @@
 # Task 3:- Logical Operators
 
+"""
+
+1. Create two boolean variables, x and y.
+2. Use logical operators (and, or, not) to perform various logical operations on x and y.
+3. print the results.
+
+"""
+x = 5
+
+result = x > 3 and x < 10 # Result is True.(If both statements are True)
+result1 = x > 3 or x < 10 # Result is True (Because if one the statements is True)
+result2 = not(x > 3 and x < 10) # Result is False (Because not is used to reverse the result)
+
+print(result)
+print(result1)
+print(result2)
+
