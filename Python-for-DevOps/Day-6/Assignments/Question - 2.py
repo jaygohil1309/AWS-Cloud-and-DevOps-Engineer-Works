05c9c42f3bba8e9daf46f2cd50c8b682dcb630be
jaygohil1309 <gohiljayb10@gmail.com>
2024-04-24 22:38:35 +0530

    add changes

---
+++ Python-for-DevOps/Day-6/Assignments/Question - 2.py
@@ -0,0 +1,15 @@
+# Task 2:- Comparison Operations
+
+"""
+
+1. Compare the values of a and b using the following comparison operations: <,>,<=,>=, ==
+   and !=.
+2. Print the results of each comparison.
+
+"""
+
+a = 10
+b = 5
+
+result = a > b # Result is True
+print(result)
