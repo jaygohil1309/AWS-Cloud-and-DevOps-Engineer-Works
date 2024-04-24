8f2bb7ef50d61256149eb57cde8813591aedb0ba
jaygohil1309 <gohiljayb10@gmail.com>
2024-04-24 22:27:17 +0530

    add new changes

--- Python-for-DevOps/Day-6/Assignments/Question - 1.py
+++ Python-for-DevOps/Day-6/Assignments/Question - 1.py
@@ -16,5 +16,12 @@
 a = 10
 b = 5
 
-result = a + b
+result = a + b # Sum
+result1 = a - b # Difference
+result2 = a * b # Product
+result3 = a / b # quotient
+
 print(result)
+print(result1)
+print(result2)
+print(result3)
