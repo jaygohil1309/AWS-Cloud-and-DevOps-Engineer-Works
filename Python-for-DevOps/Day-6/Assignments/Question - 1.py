b419573dc65733a483eb8631219b0689a95670c5
jaygohil1309 <gohiljayb10@gmail.com>
2024-04-24 22:24:20 +0530

    add new changes

---
+++ Python-for-DevOps/Day-6/Assignments/Question - 1.py
@@ -0,0 +1,20 @@
+"""
+
+*** Python Operations Assignments :-
+
+"""
+# Task 1:- Arithmetic Operations
+
+"""
+
+1. Create two variables a and b with numeric values.
+2. Calculate the sum, difference, product and quotient of a and b.
+3. print results.
+
+"""
+
+a = 10
+b = 5
+
+result = a + b
+print(result)
