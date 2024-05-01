81ef310249e67adb7fbf0e3cccdac312dcaf5966
jaygohil1309 <gohiljayb10@gmail.com>
2024-05-01 22:34:51 +0530

    add new changes

--- Python-for-DevOps/Day-6/Assignments/Question - 6.py
+++ Python-for-DevOps/Day-6/Assignments/Question - 6.py
@@ -5,6 +5,7 @@
 
 1. Create a list my_list containing a few  elements.
 2. Use identity operators (is and is not) to check if two variables are the same object.
-
+3. Use membership operators (in and not in) to check if an element is present in my_list.
+4. Print the results.
 
 """
