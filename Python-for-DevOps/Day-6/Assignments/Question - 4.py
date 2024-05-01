5c3154f9b3967362b23a3b07d41393db2c983390
jaygohil1309 <gohiljayb10@gmail.com>
2024-05-01 22:16:47 +0530

    add new changes

--- Python-for-DevOps/Day-6/Assignments/Question - 4.py
+++ Python-for-DevOps/Day-6/Assignments/Question - 4.py
@@ -1,2 +1,11 @@
 
 # Task 4: Assignment Operators
+
+"""
+
+1. Create a varible total and initialize it to 10.
+2. Use assignment operations (+=, -=, *=, /=) to update the value of total.
+3. Print the final value of total.
+
+"""
+
