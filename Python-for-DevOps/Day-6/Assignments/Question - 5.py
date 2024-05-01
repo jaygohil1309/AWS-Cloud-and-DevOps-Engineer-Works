e600ad90a725f41f287387b7f39af07816e4d4cc
jaygohil1309 <gohiljayb10@gmail.com>
2024-05-01 22:25:27 +0530

    add new changes

--- Python-for-DevOps/Day-6/Assignments/Question - 5.py
+++ Python-for-DevOps/Day-6/Assignments/Question - 5.py
@@ -1,2 +1,9 @@
 
-# 
+# Task 5: Bitwise Operators (Optional)
+
+"""
+
+1. If you are comfortable with bitwise operators, perform some bitwise operations on
+   Integer values and print the results
+
+"""
