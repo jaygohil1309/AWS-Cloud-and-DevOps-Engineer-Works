67bccba2c50f04a1f38a028307dc93be2b7f6329
jaygohil1309 <gohiljayb10@gmail.com>
2024-08-06 22:23:42 +0530

    add new changes

--- Python-for-DevOps/Python_Practice/Types of Operators.py
+++ Python-for-DevOps/Python_Practice/Types of Operators.py
@@ -1,23 +1,38 @@
 # Arithmetic Operators :- 
 
-a = 10
-b = 2
+# a = 10
+# b = 2
 
 # sum = a + b
 
-print(a + b) # Addition
-print(a - b) # Subtraction
-print(a * b) # Multiplication
-print(a / b) # Division
-print(a % b) # Moduler
-print(a ** b) # Power Using for Squere and Cube of any numbers.
+# print(a + b) # Addition
+# print(a - b) # Subtraction
+# print(a * b) # Multiplication
+# print(a / b) # Division
+# print(a % b) # Moduler
+# print(a ** b) # Power Using for Squere and Cube of any numbers.
 
 # Relational Operators :-
 
-a = 50
-b = 10
+# a = 50
+# b = 10
 
-print(a == b) # False
-print(a != b) # True
+# print(a == b) # False
+# print(a != b) # True
+# print(a >= b) # True
+# print(a > b) # True
+# print(a <= b) # False
+# print(a < b) # False
+
+# Assignment Operators :-
+
+num = 10
+# num = num + 10
+# num += 10
+# num -= 10
+# num *= 10
+# num /= 10
+num **= 5
+print("num :", num)
 
 
