e0400dcafd6bbdbe517ed1339d0dbc816ee118eb
jaygohil1309 <gohiljayb10@gmail.com>
2024-08-07 21:52:07 +0530

    add changes

--- Python-for-DevOps/Python_Practice/String.py
+++ Python-for-DevOps/Python_Practice/String.py
@@ -1,10 +1,28 @@
-a = "Hello"
-b = "World!"
 
-str = a + " " + b # Concatenation
+# # 1) Concatenation and Length :-
+
+# a = "Hello"
+# b = "World!"
+
+# # str = a + " " + b # Concatenation
+# str = a + b
+# print(str)
+# print(len(str)) # Length
+
+# ' Single Quote '
+# " Double Quote "
+# """ Triple Quote """
+
+# str1 = "My name is Jay Gohil.\nI am from kodinar." # Used for Create a new line.........
+# str2 = "I am a AWS and DevOps Engineer.\tMy company name is The One Technologies." # Used for Create a space between two lines.
+
+# print(str1)
+# print(str2)
+
+# 2) String_Indexing :-
+
+str = "Apna_College"
+str[2] = "n" # TypeError: 'str' object does not support item assignment....
+# char = str[4]
+# print(str[2])
 print(str)
-print(len(str)) # Length
-
-' Single Quote '
-" Double Quote "
-""" Triple Quote """
