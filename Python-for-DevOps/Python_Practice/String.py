11b0c6a8fc933147abafc52793a0e1bc98451c0b
jaygohil1309 <gohiljayb10@gmail.com>
2024-08-07 23:25:42 +0530

    add changes

--- Python-for-DevOps/Python_Practice/String.py
+++ Python-for-DevOps/Python_Practice/String.py
@@ -26,18 +26,46 @@
 # str[2] = "n" # TypeError: 'str' object does not support item assignment....
 # # char = str[4]
 # # print(str[2])
-# print(str)
+# # print(str)
 
  
-# String_Slicing :-
+# # String_Slicing :-
 
-str = "Apna College"
-new_str = str[1:4] # pna
-new_str1 = str[:4] # Apna [0:4]
-new_str2 = str[5:] # pna [5:len(str)]
+# str = "Apna College"
+# new_str = str[1:4] # pna
+# new_str1 = str[:4] # Apna [0:4]
+# new_str2 = str[5:] # pna [5:len(str)]
 
-print(str[5:len(str)]) # Important....
+# print(str[5:len(str)]) # Important....
 
-print(new_str2)
-print(new_str1)
-print(new_str)
+# print(new_str2)
+# print(new_str1)
+# print(new_str)
+
+# Slicing with Negative Index :-
+
+# str = "Apple"
+
+# print(str[-3:-1]) # pl
+
+
+# String Functions :-
+str = "I am a Coder."
+
+str1 = str.endswith("er.") # return True if string ends with substr.....
+print(str1)
+
+str2 = str.capitalize() # capitalize 1st char.....
+print(str2)
+
+str3 = str.upper() # Convert into Upper_Case all the string.....
+print(str3)
+
+str4 = str.replace("Coder", "Cloud and DevOps Engineer") # replaces all occurrences of Old with New.....
+print(str4)
+
+str5 = str.find() # returns 1st index of 1st occurrence.....
+print(str5)
+
+str6 = str.count("Coder") # Counts the occurrence of substr in string.....
+print(str6) 
