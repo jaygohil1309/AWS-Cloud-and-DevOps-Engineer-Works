1db7eb446307aa7be0357094251fe9aae4b6461b
jaygohil1309 <gohiljayb10@gmail.com>
2024-08-08 07:27:44 +0530

    add changes

---
+++ Python-for-DevOps/Python_Practice/Conditional_Statements.py
@@ -0,0 +1,13 @@
+"""
+
+1) If_______Else.....
+2) If___Elif___Else.....
+3) Nested___If___Else.....
+
+"""
+age = 21
+
+if(age >= 18):
+    print("You are eligible for vote and licence")
+else:
+    print("Not eligible")
