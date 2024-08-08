9263f35e983c9ca512042322b95aa6d420991e8d
jaygohil1309 <gohiljayb10@gmail.com>
2024-08-08 07:32:53 +0530

    add changes

--- Python-for-DevOps/Python_Practice/Conditional_Statements.py
+++ Python-for-DevOps/Python_Practice/Conditional_Statements.py
@@ -5,9 +5,22 @@
 3) Nested___If___Else.....
 
 """
-age = 21
+# 1) IF____Else........
 
-if(age >= 18):
-    print("You are eligible for vote and licence")
+# age = 21
+
+# if(age >= 18):
+#     print("You are eligible for vote and licence")
+# else:
+#     print("Not eligible")
+
+# 2) Else____If.......
+
+Light = input("Enter all the Signle lights :")
+
+if(Light == "Green"):
+    print("Go")
+elif(Light == "Yellow"):
+    print("Look")
 else:
-    print("Not eligible")
+    print("Stop")
