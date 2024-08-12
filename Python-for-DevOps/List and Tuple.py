d18fcee4addbbe5bc14df7bb18fcdebf36966730
jaygohil1309 <gohiljayb10@gmail.com>
2024-08-12 22:23:17 +0530

    add new changes

--- Python-for-DevOps/List and Tuple.py
+++ Python-for-DevOps/List and Tuple.py
@@ -1,11 +1,15 @@
-mark1 = 98
-mark2 = 95
-mark3 = 89
-mark4 = 87
+# mark1 = 98
+# mark2 = 95
+# mark3 = 89
+# mark4 = 87
 
-list = [mark1,mark2,mark3,mark4]
+# list = [mark1,mark2,mark3,mark4]
 
-marks = []
-for mark in list:
-    marks.append(mark)
-print(marks)
+# marks = []
+# for mark in list:
+#     marks.append(mark)
+# print(marks)
+
+marks = [85, 94, 76, 63, 48]
+
+print(marks[1:4]) # Slicing_Method
