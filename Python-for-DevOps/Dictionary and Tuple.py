7d6fb305c2a42231bafefbf24c83e61d59bed4f7
jaygohil1309 <gohiljayb10@gmail.com>
2024-08-13 22:18:07 +0530

    add changes

--- Python-for-DevOps/Dictionary and Tuple.py
+++ Python-for-DevOps/Dictionary and Tuple.py
@@ -1,5 +1,8 @@
 # Dictionary in Python :- Dictionary accepting all the data-types.
 
+# Int, Float, Boolean, List etc.........
+# Unindexable suppoter....
+
 dict = {
     'key' : "value",
     'name' : "apnacollege",
@@ -8,4 +11,8 @@ dict = {
     'is_adult' : True,
     'marks' : 94.4
 }
+dict["name"] = "Jay Gohil" # This is Important in python_dict concept.....
 print(dict)
+
+# print(dict)
+# print(type(dict))
