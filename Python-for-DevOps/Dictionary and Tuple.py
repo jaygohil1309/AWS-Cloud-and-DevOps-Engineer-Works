6f20f83bdb8501d26ca94efde553d5133e16d527
jaygohil1309 <gohiljayb10@gmail.com>
2024-08-13 22:20:15 +0530

    add changes

--- Python-for-DevOps/Dictionary and Tuple.py
+++ Python-for-DevOps/Dictionary and Tuple.py
@@ -3,16 +3,22 @@
 # Int, Float, Boolean, List etc.........
 # Unindexable suppoter....
 
-dict = {
-    'key' : "value",
-    'name' : "apnacollege",
-    'learning' : "coding",
-    'age' : 22,
-    'is_adult' : True,
-    'marks' : 94.4
-}
-dict["name"] = "Jay Gohil" # This is Important in python_dict concept.....
-print(dict)
+# dict = {
+#     'key' : "value",
+#     'name' : "apnacollege",
+#     'learning' : "coding",
+#     'age' : 22,
+#     'is_adult' : True,
+#     'marks' : 94.4
+# }
+# dict["name"] = "Jay Gohil" # This is Important in python_dict concept.....
+# print(dict)
 
 # print(dict)
 # print(type(dict))
+
+# Null_Dictionary :-
+
+null_dict = {}
+null_dict['name'] = "apnacollege"
+print(null_dict)
